667812c8057c846d0caa37954c4f4826453f7f90
Chrissy Quinter <chrissyquinter@gmail.com>
2018-03-27 15:15:01 -0400

    formatting and adding hierarchy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -45,7 +45,7 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :title)
+    params.require(:user).permit(:first_name, :last_name, :title, :manager_id)
   end
 
 end
