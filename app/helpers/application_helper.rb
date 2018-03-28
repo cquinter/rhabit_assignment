dd6bd0cd9b2b9d7bf2a413cb8b37f22d6c8ff91b
Chrissy Quinter <chrissyquinter@gmail.com>
2018-03-27 21:04:52 -0400

    created helper method to refactor index view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,15 @@
 module ApplicationHelper
 
+  def display_employees_for(employee_passed_in, results="")
+    results << "<ul>"
+    employee_passed_in.employees.each do |employee|
+      results << "<li>#{link_to employee.full_name, user_url(employee.id)}, #{employee.title}</li>"
+      if employee.employees.present?
+        results << display_employees_for(employee)
+      end
+    end
+    results << "</ul>"
+    results.html_safe
+  end
+
 end
